a83c89a30693974a65a27430d87fdaf3dbc7e837
Adam Karpita <adam@karpita.cz>
2021-07-22 18:27:34 +0200

    Odstranění __destruct a reformatování kodu

--- XmlData.php
+++ XmlData.php
@@ -6,29 +6,26 @@ namespace Xml;
 
 class XmlData
 {
-   /** @var Dibi\Connection  */
-   public $database;
+	/** @var Dibi\Connection */
+	public $database;
 
 
-   public function __construct($database) {
-       $this->database = $database;
-   }
+	public function __construct($database) {
+		$this->database = $database;
+	}
 
-   public function __destruct() {
 
-   }
+	/**
+	 * @return Dibi\Connection
+	 */
+	public function getDatabase() {
+		return $this->database;
+	}
 
-    /**
-     * @return Dibi\Connection
-     */
-    public function getDatabase() {
-        return $this->database;
-    }
+	public function installTableZaznamy() {
+		$database = $this->getDatabase();
 
-   public function installTableZaznamy() {
-        $database = $this->getDatabase();
-
-        $database->query('
+		$database->query('
             CREATE TABLE IF NOT EXISTS `zaznamy` (
             id INT(10) NOT NULL AUTO_INCREMENT,
             jmeno VARCHAR(64) NOT NULL,
@@ -36,28 +33,27 @@ class XmlData
             datum DATE,
             PRIMARY KEY (id))
         ');
-   }
+	}
 
 
-   public function loadDataFromXml ($file): \SimpleXMLElement|bool|string|null
-   {
-        return simplexml_load_file($file);
-   }
+	public function loadDataFromXml($file): \SimpleXMLElement|bool|string|null {
+		return simplexml_load_file($file);
+	}
 
-   public function instertDataIntoTableZaznamy ($values) {
-        $database = $this->getDatabase();
+	public function instertDataIntoTableZaznamy($values) {
+		$database = $this->getDatabase();
 
-        $database->query('INSERT INTO `zaznamy` %v', $values);
-   }
+		$database->query('INSERT INTO `zaznamy` %v', $values);
+	}
 
-   public function getDataFromTableZaznamy () {
-        $database = $this->getDatabase();
+	public function getDataFromTableZaznamy() {
+		$database = $this->getDatabase();
 
-        return $database->query('
+		return $database->query('
             SELECT id, jmeno, prijmeni, datum 
             FROM `zaznamy`
             ORDER BY datum
         ');
-   }
+	}
 
 }
